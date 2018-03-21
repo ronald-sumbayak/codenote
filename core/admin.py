5e7e70a261c7b092887106361e3973e042b8723d
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2018-03-21 18:56:19 +0700

    Add CompilerAdmin

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from .models import Compiler, Submission, Code, Reserved
 
 admin.site.register ([
-    Compiler,
     Submission,
     Code,
     Reserved])
+
+
+@admin.register (Compiler)
+class CompilerAdmin (admin.ModelAdmin):
+    list_display = ['name', 'id', 'mime', 'highlights', 'codemirror']
