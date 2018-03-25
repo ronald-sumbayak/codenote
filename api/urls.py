5cd4eefebf7f7b4c9148e9872018f592b2c495be
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2018-03-26 03:42:53 +0700

    Simplifiy url configs naming (+ url configs in previous commit)

--- api/urls.py
+++ api/urls.py
@@ -3,12 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path ('refresh_compilers', views.RefreshCompilerList.as_view (), name='refresh_compilers'),
+    path ('refresh', views.RefreshCompiler.as_view (), name= 'refresh'),
+    path ('code', views.RetrieveCode.as_view (), name='code'),
+    path ('check', views.Check.as_view (), name='check'),
+    path ('upload', views.Upload.as_view (), name='upload-api'),
     path ('rename', views.Rename.as_view (), name='rename'),
     path ('lock', views.Lock.as_view (), name='lock'),
     path ('unlock', views.Unlock.as_view (), name='unlock'),
-    path ('check_password', views.CheckPassword.as_view (), name='check_password'),
+    path ('password', views.CheckPassword.as_view (), name='password'),
     path ('update', views.Update.as_view (), name='update'),
     path ('compile', views.Compile.as_view (), name='compile'),
-    path ('check_submission', views.CheckSubmission.as_view (), name='check_submission'),
+    path ('submission', views.CheckSubmission.as_view (), name='submission'),
 ]
